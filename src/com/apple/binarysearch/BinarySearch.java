27787ab56e48618fa1bba50d1fdd9aa5ca1c9fae
inmin123 <zhengmincy1115@163.com>
2019-09-25 18:13:44 +0800

    created a binarysearch class

---
+++ src/com/apple/binarysearch/BinarySearch.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+class ArrayBinarySearch {
+	public static void main(String args[]) {
+		int a[] = {1,2,3,4,5},i;
+		i = Arrays.binarySearch(a,67);
+		System.out.println(i);
+	}
+}
