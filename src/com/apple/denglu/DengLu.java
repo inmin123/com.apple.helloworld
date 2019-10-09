f79084105e5fe4a6dd4a5cfb4791524d57ca8869
inmin123 <zhengmincy1115@163.com>
2019-10-09 16:54:02 +0800

    created a denglu class

---
+++ src/com/apple/denglu/DengLu.java
@@ -0,0 +1,11 @@
+public static void main(String[] atgs) {
+	final String username = "test";
+	final String userpassword = "123456";
+	User user = new User();
+	Scanner scanner = new Scanner(System.in);
+	String usernameinput = scanner.next();
+	if(username.equals(usernameinput)) {
+		System.out.println("Welcome!");
+		if()
+	}
+}
